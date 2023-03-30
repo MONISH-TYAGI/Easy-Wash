b807f207d94fde1cb6165025187380da2d6df62d
Monish <monishtyagi5770@gmail.com>
2023-03-30 20:12:47 +0530

    Navbar Bold

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -59,17 +59,17 @@ function Navbar() {
      </div>
      <div className='h-full w-2/4  flex  z-1'>
      
-       <div className='h-full w-1/6  flex items-center justify-center hover:text-select cursor-pointer' onClick={handleHome}><i className="fa-solid fa-house "></i><span>Home</span></div>
-       <div className='h-full w-1/6  flex items-center justify-center hover:text-select cursor-pointer' onClick={handleServices}><i className="fa-solid fa-gear "></i><span>Services</span></div>
-       <div className='h-full w-1/6  flex items-center justify-center hover:text-select  cursor-pointer' onClick={handleReviews}><i className="fa-solid fa-heart "></i><span>Reviews</span></div>
-       <div className='h-full w-1/6 flex items-center justify-center hover:text-select cursor-pointer' onClick={handleBag}><i className="fa-solid fa-bag-shopping "></i><span>Bag</span></div>
+       <div className='h-full w-1/6  flex items-center justify-center hover:text-select cursor-pointer mr-1' onClick={handleHome}><i className="fa-solid fa-house text-lg "></i><span className='text-2xl ' style={{"font-family": "-webkit-body"}}>Home</span></div>
+       <div className='h-full w-1/6  flex items-center justify-center hover:text-select cursor-pointer mr-2' onClick={handleServices}><i className="fa-solid fa-gear text-lg"></i><span  className='text-2xl ' style={{"font-family": "-webkit-body"}}>Services</span></div>
+       <div className='h-full w-1/6  flex items-center justify-center hover:text-select  cursor-pointer mr-1 ml-2' onClick={handleReviews}><i className="fa-solid fa-heart text-lg"></i><span  className='text-2xl ' style={{"font-family": "-webkit-body"}}>Reviews</span></div>
+       <div className='h-full w-1/6 flex items-center justify-center hover:text-select cursor-pointer mr-1' onClick={handleBag}><i className="fa-solid fa-bag-shopping text-lg"></i><span  className='text-2xl ' style={{"font-family": "-webkit-body"}}>Bag</span></div>
        
         
         <div className='h-full w-2/6  flex  items-center justify-center hover:text-select cursor-pointer'  onMouseEnter={()=>setIcon(true)} onMouseLeave={()=>setIcon(false)}>
           {
           (icon==false||name=="LogIn")?
           <span onMouseEnter={()=>setIcon(true)} onClick={goToLoginPage} onMouseLeave={()=>setIcon(false)} >
-          <i className="fa-solid fa-user m-1 " ></i>{name}</span>:<span onMouseLeave={()=>setIcon(false)} onClick={handleLogout}><i class="fa-solid fa-lock-open pr-1"></i>LogOut</span>
+          <i className="fa-solid fa-user m-1 text-xl" ></i><span className='font-bold'>{name}</span></span>:<span className="text-xl" onMouseLeave={()=>setIcon(false)} onClick={handleLogout}><i class="fa-solid fa-lock-open pr-1 text-base text-xl"></i><span className='font-bold'>LogOut</span></span>
 }
 {/* <i class="ml-3   fa-sharp fa-solid fa-caret-down" onClick={handleMenu}></i> */}
           </div>
